1232ff3535b8506846f73d09a0e91da735956804
Sayem Hossen <sayemhossen068@gmail.com>
2026-02-06 19:54:46 +0600

    Binary Search without user input

---
+++ DSA/Searching/C/Binary_Search.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int binarySearch (int arr[], int n, int key) {
+    int beg = 0;
+    int end = n - 1;
+    
+    while (beg <= end) {
+        int mid = (beg + end) / 2;
+
+        if(arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] < key) {
+            beg = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main () {
+    int arr[] = {6, 10, 20, 30, 55, 75, 190};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key;
+    printf("Enter the key to search: ");
+    scanf("%d", &key);
+
+    int index = binarySearch(arr, n, key);
+    if(index != -1) {
+        printf("Element found at index: %d\n", index);
+    } else {
+        printf("Element not found.\n");
+    }
+}
